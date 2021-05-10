36d782f2c027d8e07c1c8c5fdf3e22e2d8982b6e
Gabriel Matute <gmatute@berkeley.edu>
2021-05-10 16:05:21 -0400

    Add simplify example with an or.

--- demo.ml
+++ demo.ml
@@ -226,6 +226,7 @@ module SimplifyExamples = struct
     ; "{ <a:[<>], b:[<>], c:[<>]> | 2len val.a + 3len val.b <= 5len val.c}"
     ; "{ <a:[<>], b:[<>], c:[<>]> | 5len val.a <= 2len val.b + 3len val.c}"
     ; "{ { <a:[<>], b:[<>]> | 1len val.a <= 1len val.b} | 1len val.b <= 1len val.a}"
+    ; "{ <a:[<>], b:[<>], c:[<>]> | 1len val.a <= 1len val.b V 1len val.a <= 1len val.c }"
     ]
 
 
